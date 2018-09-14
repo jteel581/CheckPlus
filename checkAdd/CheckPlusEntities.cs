482e5a5d5eaf10e1438b81a1c44a821d109f9a57
i_mora_les <imora310@outlook.com>
2018-09-14 05:25:54 -0400

    Finished up adding all entity objects to the solution; still need to write the accessor functions

--- checkAdd/CheckPlusEntities.cs
+++ checkAdd/CheckPlusEntities.cs
@@ -15,26 +15,7 @@ using System.Data.Common;
 using System.Text.RegularExpressions;
 
 namespace CheckPlusEntities
-{
-    [Table("entity_type")]
-    class Entity_type
-    {
-        [Key]
-        public int Entity_type_id { get; set; }
-        public string Entity_type_nm { get; set; }
-    }
-
-    [Table("entity")]
-    class Entity
-    {
-        [Key]
-        public int Entity_id { get; set; }
-
-        [ForeignKey("entity_type")]
-        public int Entity_type_id { get; set; }
-        public Entity_type Entity_type { get; set; }
-    }
-
+{   //in alphabetical order by table name (at least, that's the goal)
     [Table("account")]
     class Account
     {
@@ -51,7 +32,6 @@ namespace CheckPlusEntities
 
         public string Account_number { get; set; }
         public string Routing_number { get; set; }
-
         public DateTime Date_start { get; set; }
         public DateTime? Date_end { get; set; }
     }
@@ -70,6 +50,107 @@ namespace CheckPlusEntities
         public DateTime Date_received { get; set; }
     }
 
+    [Table("account_type")]
+    class Account_type
+    {
+        [Key]
+        public int Account_type_id { get; set; }
+        public string Account_type_nm { get; set; }
+    }
+
+    [Table("addr_type")]
+    class Addr_type
+    {
+        [Key]
+        public int Addr_type_id { get; set; }
+        public string Addr_type_cd { get; set; }
+        public string Addr_type_nm { get; set; }
+    }
+
+    [Table("address")]
+    class Address
+    {
+        [Key]
+        public int Address_id { get; set; }
+
+        [ForeignKey("citystate")]
+        public int Citystate_id { get; set; }
+        public Citystate Citystate { get; set; }
+
+        [ForeignKey("addr_type")]
+        public int Addr_type_id { get; set; }
+        public Addr_type Addr_Type { get; set; }
+
+        public string Address_nm { get; set; }
+    }
+
+    [Table("citystate")]
+    class Citystate
+    {
+        [Key]
+        public int Citystate_id { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string State_code { get; set; }
+        public string Country { get; set; }
+        public string Postal_code { get; set; }
+    }
+
+    [Table("ent_addr_rel")]
+    class Ent_addr_rel
+    {
+        [Key]
+        public int Ent_addr_rel_id { get; set; }
+
+        [ForeignKey("entity")]
+        public int Entity_id { get; set; }
+        public Entity Entity { get; set; }
+
+        [ForeignKey("address")]
+        public int Address_id { get; set; }
+        public Address Address { get; set; }
+        public DateTime Date_start { get; set; }
+        public DateTime? Date_end { get; set; }
+    }
+
+    [Table("entity")]
+    class Entity
+    {
+        [Key]
+        public int Entity_id { get; set; }
+
+        [ForeignKey("entity_type")]
+        public int Entity_type_id { get; set; }
+        public Entity_type Entity_type { get; set; }
+    }
+
+
+    [Table("entity_type")]
+    class Entity_type
+    {
+        [Key]
+        public int Entity_type_id { get; set; }
+        public string Entity_type_nm { get; set; }
+    }
+
+    [Table("letter")]
+    class Letter
+    {
+        [Key]
+        public int Letter_id { get; set; }
+
+        [ForeignKey("account_check")]
+        public int Account_check_id { get; set; }
+        public Account_check Account_check { get; set; }
+
+        [ForeignKey("letter_stage")]
+        public int Letter_stage_id { get; set; }
+        public Letter_stage Letter_stage { get; set; }
+
+        public DateTime? Date_sent { get; set; }
+        public DateTime? Date_response { get; set; }
+    }
+
     [Table("leter_stage")]
     class Letter_stage
     {
@@ -79,21 +160,47 @@ namespace CheckPlusEntities
         public string Default_text { get; set; }
     }
 
-    [Table("letter")]
-    class Letter
+    [Table("organization")]
+    class Organization
     {
         [Key]
-        public int Letter_id { get; set; }
-        
-        [ForeignKey("account_check")]
-        public int Account_check_id { get; set; }
-        public Account_check Account_check { get; set; }
+        [ForeignKey("entity")]
+        public int Organization_id { get; set; }
+        public Entity Entity { get; set; }
 
-        [ForeignKey("letter_stage")]
-        public int Letter_stage_id { get; set; }
-        public Letter_stage Letter_stage { get; set; }
+        public string Organization_nm { get; set; }
+    }
 
-        public DateTime Date_sent { get; set; }
-        public DateTime? Date_response { get; set; }
+    [Table("Person")]
+    class Person
+    {
+        [Key]
+        [ForeignKey("entity")]
+        public int Person_id { get; set; }
+        public Entity Entity { get; set; }
+
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public string Middle_name { get; set; }
+        public string Suffix { get; set; }
+        public string Title { get; set; }
+    }
+
+    class CheckPlusDB : DbContext
+    {
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Account_check> Account_Checks { get; set; }
+        public virtual DbSet<Account_type> Account_Types { get; set; }
+        public virtual DbSet<Addr_type> Addr_Types { get; set; }
+        public virtual DbSet<Address> Addresses { get; set; }
+        public virtual DbSet<Citystate> Citystates { get; set; }
+        public virtual DbSet<Ent_addr_rel> Ent_Addr_Rels { get; set; }
+        public virtual DbSet<Entity> Entities { get; set; }
+        public virtual DbSet<Entity_type> Entity_Types { get; set; }
+        public virtual DbSet<Letter> Letters { get; set; }
+        public virtual DbSet<Letter_stage> Letter_Stages { get; set; }
+        public virtual DbSet<Organization> Organizations { get; set; }
+        public virtual DbSet<Person> People { get; set; }
+
     }
 }
