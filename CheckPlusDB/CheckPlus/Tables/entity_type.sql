82731d175ed41083d6567460a826a05e3d418fb9
imora310 <imora310@students.bju.edu>
2018-09-18 16:56:52 -0400

    Adding db schema solution to source control.

---
+++ CheckPlusDB/CheckPlus/Tables/entity_type.sql
@@ -0,0 +1,11 @@
+﻿/*
+	description:
+	notes:
+*/
+CREATE TABLE dbo.entity_type
+(
+	entity_type_id		int				not null,
+	entity_type_nm		varchar(50)		not null,					--e.g. 'Account Holder', 'Bank', 'Employee'
+primary key (entity_type_id asc)
+)
+;
---
+++ CheckPlusDB/CheckPlus/Tables/letter_stage.sql
@@ -0,0 +1,12 @@
+﻿/*
+	description:
+	notes:
+*/
+create table dbo.letter_stage
+(
+	letter_stage_id			int				not null,
+	letter_stage_nm			varchar(30)		not null,
+	default_text			varchar(1000)	null,
+primary key (letter_stage_id asc)
+)
+;
